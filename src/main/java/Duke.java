ef8aa91a6c3048f116ea4660c4e1a792217c74a6
Hong Chuan <hongchuan.nus@gmail.com>
2019-08-21 11:50:57 +0800

    level 1 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     /**
      * Main class.
@@ -10,5 +12,21 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        String horilines = "\t____________________________________________________________";
+        System.out.println(horilines);
+        System.out.println("\t Hello! I'm Duke \n\t What can I do for you?");
+        System.out.println(horilines);
+        Scanner Input = new Scanner(System.in);
+        String command = Input.nextLine();
+        while (!"bye".equals(command)){
+            System.out.println(horilines);
+            System.out.println("\t "+ command);
+            System.out.println(horilines);
+            command = Input.nextLine();
+        }
+            System.out.println(horilines);
+            System.out.println("\t Bye. Hope to see you again soon!");
+            System.out.println(horilines);
+
     }
 }
