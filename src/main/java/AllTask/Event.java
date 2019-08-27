f2b0feb2fa1189bc934e25d4b6ea7e88f4af3aad
Hong Chuan <hongchuan.nus@gmail.com>
2019-08-27 10:52:35 +0800

    fix event

--- src/main/java/AllTask/Event.java
+++ src/main/java/AllTask/Event.java
@@ -11,6 +11,6 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (by: " + date + " " + time + ")";
+        return "[E]" + super.toString() + " (at: " + date + " " + time + ")";
     }
 }
